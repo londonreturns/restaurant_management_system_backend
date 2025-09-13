530b3d062fe8d2f03eb7694d760700d5c6859f59
londonreturns <subedioscar.47@gmail.com>
2025-09-13 10:26:30 +0545

    database structure changed

--- menu/src/main/java/com/example/Repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repository;
-
-import com.example.Entity.MenuEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
-}
